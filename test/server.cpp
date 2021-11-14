53f08a9ac30e9cb1dd310ff44a15f3381622ae42
Knewhow <how.yuangh@gmail.com>
2021-11-14 16:09:18 +0800

    add close client socket

--- test/server.cpp
+++ test/server.cpp
@@ -47,6 +47,7 @@ int main(int argc, char **argv) {
                     break;
                 }
             }
+            server.CloseClient();
             google::FlushLogFiles(google::INFO);
             
         }
